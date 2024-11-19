ab56bc3ea47186726a000ffbbe46fb4003ba46b4
DanialWajid <danialwajid112@gmail.com>
2024-11-19 21:46:19 +0500

    go

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,18 @@
-//Server Here it will run here (Listen)
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./db/index.js");
+
+console.log("Before dotenv config: ", process.env.MONGODB_URI);
+dotenv.config({ path: ".env.local" });
+console.log("After dotenv config: ", process.env.MONGODB_URI);
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => res.send("hello world"));
+
+console.log("MongoDB URI: ", process.env.MONGODB_URI);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
