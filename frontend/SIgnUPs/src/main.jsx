3f9e68d7f5d0af1880114963d9ed13a176a9ffa7
DanialWajid <danialwajid112@gmail.com>
2024-11-22 00:03:04 +0500

    signup

--- frontend/SIgnUPs/src/main.jsx
+++ frontend/SIgnUPs/src/main.jsx
@@ -2,12 +2,13 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import SignApp from "./SignApp";
+import { BrowserRouter } from "react-router-dom";
 // import Signup from "./signup";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <StrictMode>
+  <BrowserRouter>
     <SignApp />
-  </StrictMode>
+  </BrowserRouter>
 );
