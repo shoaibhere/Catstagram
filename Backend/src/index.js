19d9e1e1f612ec52e800da47e45d3309a2e633d0
shoaibhere <shoaibakhtar321@icloud.com>
2025-01-27 22:29:36 +0500

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,6 +21,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // The frontend URL
     credentials: true,
   })
 );
