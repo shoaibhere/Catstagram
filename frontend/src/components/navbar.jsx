e016aeb952db98ed4c0f3c915e5a937e7e7aff30
shoaibhere <shoaibakhtar321@icloud.com>
2025-01-28 03:47:12 +0500

    Update navbar.jsx

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <a className="flex flex-shrink-0 items-center mr-4" href="/">
               <img className="h-10 w-auto rounded-full" src={logo} alt="Catstagram"/>
               <span className="text-2xl font-billabong ml-2">Catstagram</span>
             </a>
