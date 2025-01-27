9395a11a5b4a88e2b65939a4ef799cfe9f73276f
shoaibhere <shoaibakhtar321@icloud.com>
2025-01-27 22:51:35 +0500

    Update cookie.js

--- Backend/src/utils/cookie.js
+++ Backend/src/utils/cookie.js
@@ -13,7 +13,7 @@ const generateTokenSetCookie = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production"?"strict":"none",
+    sameSite: process.env.NODE_ENV === "production"?"none":"strict",
     maxAge: 7 * 24 * 60 * 60 * 1000, // Set maxAge
   });
 
